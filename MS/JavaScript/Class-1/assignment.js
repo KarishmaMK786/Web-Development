21efea69b006326250e3a120cb42b7c82e6a080e
KarishmaMK786 <matazkk786@gmail.com>
2023-05-10 18:39:47 +0530

    code runner

---
+++ MS/JavaScript/Class-1/assignment.js
@@ -0,0 +1,38 @@
+// var firstname = "Masai School";
+// console.log(firstname);
+
+// var firstname = "A Transformation in Eduction";
+// console.log(firstname);
+
+// var name="Karishma Khondunaik";
+// console.log(name);
+
+// name="Mataz Khondunaik";
+// console.log(name);
+
+// name="Mamtaj Khondunaik";
+// console.log(name);
+
+// var myname = "karishma khondunaik";
+// var age = "24";
+// console.log(name+" "+age);
+
+var src = "Student Report Card";
+var name = "karishma Khondunaik";
+var schoolname = "S.P.J.G High school";
+var grade = "12th";
+var schoolsec = "Section A";
+
+var sub1 = "          Maths            87";
+var sub2 = "          Science            80";
+var sub1 = "          English            93";
+var total = "        Toatal Marks Obtained         270/300";
+
+
+console.log("    **********" , src , "**********");
+console.log('\t');
+console.log("          Name -          ", name);
+console.log("          School Name -   ", schoolname);
+console.log("          grade -         ",grade);
+console.log("          section -       ", schoolsec);
+console.log('\t');
---
+++ MS/JavaScript/Class-1/index.js
@@ -0,0 +1,11 @@
+console.log("Hii raj");
+
+var age = 10;
+console.log(age+age);
+
+var name = "Kempi";
+console.log(name);
+
+console.log(typeof(age));
+
+console.log(typeof(name));
