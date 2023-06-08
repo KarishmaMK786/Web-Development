51371ffe56b81f191052e2e87dbf3ed4fbbc76c1
KarishmaMK786 <matazkk786@gmail.com>
2023-06-08 08:14:45 +0530

    files are added

---
+++ Acciojob/JavaScript/class-1/script.js
@@ -0,0 +1,47 @@
+var myName = "Kempi";
+console.log("hello World");
+var newString = "My Name Is " + myName;
+var myNumber = 10+20;
+myNumber = myNumber + 20;
+var flag = true;
+console.log(newString);
+console.log(myNumber);
+
+// single line comment
+
+/*
+  multiline comment
+  this is  
+*/
+
+//  Alerts
+
+alert("Your website is being is  hacked");
+
+/* inbuilt function
+
+ Built-in functions are ones for which the compiler generates inline code at compile time
+*/
+
+// var ans = prompt("please enter your name");
+// if(ans!=null){
+//     alert("answe to the prompt " + ans);
+// } else {
+//     alert("got null");
+// }
+
+// confirm gives a boolean
+
+// var ans = confirm("this website tracks your website");
+// if(ans!= false) {
+//     alert("you clicked ok");
+// }else {
+//     alert("you clicked cancel");
+// }
+
+
+var myName = "Karishma";
+var job = "Instructor";
+
+console.log("hii " + myName);
+console.log("hii", myName, job, 5);
---
+++ HTML/Thapa -Tachnical/javascript full course/index.js
@@ -0,0 +1,92 @@
+// console.log ("dumma");
+// console.log ("hello world");
+// var myname = "Karishma Mataz Khondunaik";
+// console.log (myname);
+
+// var _myName = "Vinod";
+// var 1myName = "thapa";
+// var _1myName= "Vinod";
+// var $myName = "Vinod";
+
+// var myName = "Karishma Mataz Khondunaik";
+// console.log(typeof(myName));
+// var myAge = 26;
+// console.log(typeof(myAge));
+// var iAmThapa  = true;
+// console.log(typeof(iAmThapa));
+
+// console.log(10+"20");
+// console.log(9-"45");
+// console.log("Java"+"Script");
+// console.log(" " + " ");
+// console.log(" "+ 0);
+// console.log("vinod"-"Thapa");
+// console.log(true+true);
+// console.log(true+false);
+// console.log(false + false);
+// console.log(false+true);
+// console.log(false - true);
+
+
+// var iAmUseless = null;
+// console.log(iAmUseless);
+
+// var iAmStandBy ;
+// console.log(iAmStandBy);
+
+// var myPhoneNumber = 7338575834;
+// var myName = "Karishma";
+
+// console.log(isNaN(myPhoneNumber));
+// console.log(isNaN(myName));
+// if(isNaN(myName)) {
+//     console.log("Please Eneter a valid Mobile Number");
+// }
+
+// console.log(5+20);
+
+// var x = 5;
+// var y = 5;
+// console.log(x==y);
+
+console.log(3**3);
+
+var a = 5 ; 
+var b = 10 ;
+// var temp = a;
+// a = b;
+// b = temp;
+// console.log("a value is " + a);
+// console.log("b value is " + b);
+
+
+// a = a + b ; 
+// b = a - b ;
+// a = a - b ; 
+// console.log("a value is " + a);
+// console.log("b value is " + b);
+
+// var a = 5 ;
+// var b = '5' ;
+// if (a == b) {
+//     console.log(true);
+// }
+
+
+// var tomorrow  = 'rain'; 
+ 
+// if (tomorrow == 'rain') {
+//     console.log("Take A Rain Coat");
+// } else {
+//     console.log ("leave it");
+// }
+
+var leap_year = 1900 ; 
+
+if (leap_year%4 == 0  && leap_year % 100 != 0 || leap_year % 400 == 0) {
+    console.log('leap year');
+}else {
+    console.log('not a leap year');
+}
+
+
